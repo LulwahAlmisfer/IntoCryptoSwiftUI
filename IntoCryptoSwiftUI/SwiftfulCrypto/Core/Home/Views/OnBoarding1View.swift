6d8a7f4eee9f985cfe91760b8c55a554b794be2f
lulu <lulukalmisfer@hotmail.com>
2023-01-15 11:25:04 +0300

    change to blue

--- IntoCryptoSwiftUI/SwiftfulCrypto/Core/Home/Views/OnBoarding1View.swift
+++ IntoCryptoSwiftUI/SwiftfulCrypto/Core/Home/Views/OnBoarding1View.swift
@@ -21,7 +21,7 @@ struct OnBoarding1View: View {
                     }
                 }.padding(.horizontal)
                 LottieView(filename: "99448-crypto-coins")
-                Text("Sensation of true trading").font(.title2).bold()
+                Text("Sensation of true trading").font(.title2).bold().foregroundColor(.blue)
                 Text("Get a representation of the market ")
                 Text("and monitor your wallet  ").foregroundColor(.black)
             }.foregroundColor(.theme.accent).font(.title2)
