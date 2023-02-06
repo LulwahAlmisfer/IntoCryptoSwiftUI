a94b6bed03c8f8f8d332958b0e2ecfd8eca5b1c7
lulu <lulukalmisfer@hotmail.com>
2023-02-06 13:27:05 +0300

    logic to BuyAndSellView (not perfect)

--- IntoCryptoSwiftUI/SwiftfulCrypto/Core/Detail/Views/BuySellCoinsView.swift
+++ IntoCryptoSwiftUI/SwiftfulCrypto/Core/Detail/Views/BuySellCoinsView.swift
@@ -8,11 +8,11 @@
 import SwiftUI
 
 struct BuySellCoinsView: View {
-    
+    @EnvironmentObject private var vm: HomeViewModel
     @Binding var coin: CoinModel?
     @State var method = 1
-    @State var amount = "2"
-    
+    @State var amount = ""
+    @State var oldamount = "0"
     var body: some View {
         NavigationView{
             ZStack {
@@ -25,15 +25,23 @@ struct BuySellCoinsView: View {
                             CoinImageView(coin: coin).frame(width: 50, height: 50)
                             
                         }
+                        
                         Spacer()
                         Picker(selection: $method, label: Text("Picker")) {
                             Text("buy").tag(1)
                             Text("sell").tag(2)
                         }.pickerStyle(.segmented)
                         
+                        if method == 2 {
+                            
+                            HStack {
+                                Text("Owned = ").foregroundColor(.theme.accent)
+                                Text(oldamount).foregroundColor(.theme.accent)
+                            }
+                        }
                         HStack{
                             Text("Amount")
-                            TextField("", text: $amount).textFieldStyle(.roundedBorder)
+                            TextField("enter amount", text: $amount).textFieldStyle(.roundedBorder)
                         }.padding()
                         
                         HStack{
@@ -44,19 +52,40 @@ struct BuySellCoinsView: View {
                         }.padding()
                         Spacer()
                     
-                        ButtonLocalizedStringKey(title: method == 1 ? "Buy": "Sell"){
-                            print("d")
+                ButtonLocalizedStringKey(title: method == 1 ? "Buy": "Sell"){
+                    if method == 1 {
+                        vm.updatePortfolio(coin: coin, amount:( Double(oldamount)! + Double(amount)!))
+                    } else {
+                        vm.updatePortfolio(coin: coin, amount:( Double(oldamount)! - Double(amount)!))
+                    }
                         }
                     }
                 }.padding()
                     .padding(.vertical,30)
             }
+        }.onAppear{
+            
+            updateSelectedCoin2(coin: coin!)
+            
+        }
         }
         
-    }}
+    private func updateSelectedCoin2(coin: CoinModel) {
+      
+        
+        if let portfolioCoin = vm.portfolioCoins.first(where: { $0.id == coin.id }),
+           let amount2 = portfolioCoin.currentHoldings {
+            oldamount = "\(amount2)"
+        } else {
+            oldamount = ""
+        }
+    }
+    }
+
+
     struct BuySellCoinsView2: PreviewProvider {
         static var previews: some View {
-            BuySellCoinsView(coin: .constant(dev.coin))
+            BuySellCoinsView(coin: .constant(dev.coin))  .environmentObject(dev.homeVM)
             
         }
     }
